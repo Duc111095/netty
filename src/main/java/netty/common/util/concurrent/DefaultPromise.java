c7cf185659807bb4c3206ee07616fc0c3ef3768a
Duc111095 <huynhduc111095@gmail.com>
2026-04-20 23:13:37 +0700

    add default Promise and Futuree

--- src/main/java/netty/common/util/concurrent/DefaultPromise.java
+++ src/main/java/netty/common/util/concurrent/DefaultPromise.java
@@ -2,11 +2,14 @@ package netty.common.util.concurrent;
 
 import java.util.Locale;
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
+import netty.common.util.internal.InternalThreadLocalMap;
+import netty.common.util.internal.PlatformDependent;
 import netty.common.util.internal.StringUtil;
 import netty.common.util.internal.SystemPropertyUtil;
 import netty.common.util.internal.ThrowableUtil;
@@ -32,6 +35,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	private static final Object UNCANCELLABLE = new Object();
 	private static final CauseHolder CANCELLATION_CAUSE_HOLDER = new CauseHolder(
 			StacklessCancellationException.newInstance(DefaultPromise.class, "cancel(...)"));
+	private static final StackTraceElement[] CANCELLATION_STACK = CANCELLATION_CAUSE_HOLDER.cause.getStackTrace();
 	
 	private volatile Object result;
 	private final EventExecutor executor;
@@ -99,6 +103,8 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	
 	private static final class LeanCancellationException extends CancellationException {
 		
+		private static final long serialVersionUID = 6151904822098546586L;
+
 		@Override
 		public Throwable fillInStackTrace() {
 			setStackTrace(CANCELLATION_STACK);
@@ -144,7 +150,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	}
 	
 	@Override
-	public Promise<V> addListeners(GenericFutureListener<? extends Future<? super V>>... listeners) {
+	public Promise<V> addListeners(@SuppressWarnings("unchecked") GenericFutureListener<? extends Future<? super V>>... listeners) {
 		checkNotNull(listeners, "listeners");
 		
 		synchronized (this) {
@@ -163,7 +169,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	}
 	
 	@Override
-	public Promise<V> removeListener(final GenericFutureListener<? extends Future<? extends V>> listener) {
+	public Promise<V> removeListener(final GenericFutureListener<? extends Future<? super V>> listener) {
 		checkNotNull(listener, "listener");
 		synchronized (this) {
 			removeListener0(listener);
@@ -172,7 +178,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	}
 	
 	@Override
-	public Promise<V> removeListeners(final GenericFutureListener<? extends Future<? super V>>... listeners) {
+	public Promise<V> removeListeners(@SuppressWarnings("unchecked") final GenericFutureListener<? extends Future<? super V>>... listeners) {
 		checkNotNull(listeners, "listeners");
 		
 		synchronized (this ) {
@@ -213,12 +219,12 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	}
 	
 	@Override
-	public Promise<V> awaitUninterrupted() {
+	public Promise<V> awaitUninterruptibly() {
 		if (isDone()) {
 			return this;
 		}
 		
-		checkDeadlock();
+		checkDeadLock();
 		boolean interrupted = false;
 		synchronized (this ) {
 			while (!isDone()) {
@@ -321,7 +327,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 	
 	@Override
 	public boolean cancel(boolean maybeInterruptIfRunning) {
-		if (RESULT_UPDATER.compareAndSet(this, null`, CANCELLATION_CAUSE_HOLDER)) {
+		if (RESULT_UPDATER.compareAndSet(this, null, CANCELLATION_CAUSE_HOLDER)) {
 			if (checkNotifyWaiters()) {
 				notifyListeners();
 			}
@@ -392,6 +398,351 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 		if (e != null && e.inEventLoop()) {
 			throw new BlockingOperationException(toString());
 		}
+	}
+	
+	protected static void notifyListener(
+			EventExecutor eventExecutor, final Future<?> future, final GenericFutureListener<?> listener) {
+		notifyListenerWithStackOverFlowProtection(
+				checkNotNull(eventExecutor, "eventExecutor"),
+				checkNotNull(future, "future"),
+				checkNotNull(listener, "listener"));
+	}
+	
+	private void notifyListeners() {
+		EventExecutor executor = executor();
+		if (executor.inEventLoop()) {
+			final InternalThreadLocalMap threadLocals = InternalThreadLocalMap.get();
+			final int stackDepth = threadLocals.futureListenerStackDepth();
+			if (stackDepth < MAX_LISTENER_STACK_DEPTH) {
+				threadLocals.setFutureListenerStackDepth(stackDepth + 1);
+				try {
+					notifyListenersNow();
+				} finally {
+					threadLocals.setFutureListenerStackDepth(stackDepth);
+				}
+				return;
+			}
+		}
+		
+		safeExecute(executor, new Runnable() {
+			@Override
+			public void run() {
+				notifyListenersNow();
+			}
+		});
+	}
+	
+	private static void notifyListenerWithStackOverFlowProtection(
+			final EventExecutor executor,
+			final Future<?> future,
+			final GenericFutureListener<?> listener) {
+		if (executor.inEventLoop()) {
+			final InternalThreadLocalMap threadLocals = InternalThreadLocalMap.get();
+			final int stackDepth = threadLocals.futureListenerStackDepth();
+			if (stackDepth < MAX_LISTENER_STACK_DEPTH) {
+				threadLocals.setFutureListenerStackDepth(stackDepth + 1);
+				try {
+					notifyListener0(future, listener);
+				} finally {
+					threadLocals.setFutureListenerStackDepth(stackDepth);
+				}
+				return;
+			}
+		}
+		
+		safeExecute(executor, new Runnable() {
+			@Override
+			public void run() {
+				notifyListener0(future, listener);
+			}
+		});
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private void notifyListenersNow() {
+		GenericFutureListener listener;
+		DefaultFutureListeners listeners;
+		synchronized (this) {
+			listener = this.listener;
+			listeners = this.listeners;
+			if (notifyingListeners || (listener == null && listeners == null)) {
+				return;
+			}
+			notifyingListeners = true;
+			if (listener != null) {
+				this.listener = null;
+			} else {
+				this.listeners = null;
+			}
+		}
+		for (;;) {
+			if (listener != null) {
+				notifyListener0(this, listener);
+			} else {
+				notifyListeners0(listeners);
+			}
+			synchronized (this) {
+				if (this.listener == null && this.listeners == null) {
+					notifyingListeners = false;
+					return;
+				}
+				listener = this.listener;
+				listeners = this.listeners;
+				if (listener != null) {
+					this.listener = null;
+				} else {
+					this.listeners = null;
+				}
+			}
+		}
+	}
+	
+	private void notifyListeners0(DefaultFutureListeners listeners) {
+		GenericFutureListener<?>[] a = listeners.listeners();
+		int size = listeners.size();
+		for (int i = 0; i < size; i++) {
+			notifyListener0(this, a[i]);
+		}
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static void notifyListener0(Future future, GenericFutureListener l) {
+		try {
+			l.operationComplete(future);
+		} catch (Throwable t) {
+			if (logger.isWarnEnabled()) {
+				logger.warn("An exeception was thrown by " + l.getClass().getName() + ".operationComplete()");
+			}
+		}
+	}
+	
+	private void addListener0(GenericFutureListener<? extends Future<? super V>> listener) {
+		if (this.listener == null) {
+			if (listeners == null) {
+				this.listener = listener;
+			} else {
+				listeners.add(listener);
+			}
+		} else {
+			assert listeners == null;
+			listeners = new DefaultFutureListeners(this.listener, listener);
+			this.listener = null;
+		}
+	}
+	
+	private void removeListener0(GenericFutureListener<? extends Future<? super V>> toRemove) {
+		if (listener == toRemove) {
+			listener = null;
+		} else if (listeners != null) {
+			listeners.remove(toRemove);
+			if (listeners.size() == 0) {
+				listeners = null;
+			}
+		}
+	}
+	
+	private boolean setSuccess0(V result ) {
+		return setValue0(result == null ? SUCCESS : result);
+	}
+	
+	private boolean setFailure0(Throwable cause) {
+		return setValue0(new CauseHolder(checkNotNull(cause, "cause")));
+	}
+	
+	private boolean setValue0(Object objResult) {
+		if (RESULT_UPDATER.compareAndSet(this, null, objResult) ||
+			RESULT_UPDATER.compareAndSet(this, UNCANCELLABLE, objResult)) {
+			if (checkNotifyWaiters()) {
+				notifyListeners();
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	private synchronized boolean checkNotifyWaiters() {
+		if (waiters > 0) {
+			notifyAll();
+		}
+		return listener != null || listeners != null;
+	}
+	
+	private void incWaiters() {
+		if (waiters == Short.MAX_VALUE) {
+			throw new IllegalStateException("too many waiters: " + this);
+		}
+		++waiters;
+	}
+	
+	private void decWaiters() {
+		--waiters;
+	}
+	
+	private void rethrowIfFailed() {
+		Throwable cause = cause();
+		if (cause == null) {
+			return;
+		}
+		if (!(cause instanceof CancellationException) && cause.getSuppressed().length == 0) {
+			cause.addSuppressed(new CompletionException("Rethrowing promise failure cause", null));
+		}
+		PlatformDependent.throwException(cause);
+	}
+	
+	private boolean await0(long timeoutNanos, boolean interruptable) throws InterruptedException {
+		if (isDone()) {
+			return true;
+		}
+		if (timeoutNanos <= 0) {
+			return isDone();
+		}
+		
+		if (interruptable && Thread.interrupted()) {
+			throw new InterruptedException(toString());
+		}
+		
+		checkDeadLock();
+		
+		final long startTime = System.nanoTime();
+		synchronized (this) {
+			boolean interrupted = false;
+			try {
+				long waitTime = timeoutNanos;
+				while (!isDone() && waitTime > 0) {
+					incWaiters();
+					try {
+						wait(waitTime / 1000000, (int) (waitTime % 1000000));
+					} catch (InterruptedException e) {
+						if (interruptable) {
+							throw e;
+						} else {
+							interrupted = true;
+						}
+					} finally {
+						decWaiters();
+					}
+					if (isDone()) {
+						return true;
+					}
+					waitTime = timeoutNanos - (System.nanoTime() - startTime);
+				}
+				return isDone();
+			} finally {
+				if (interrupted) {
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	void notifyProgressiveListeners(final long progress, final long total) {
+		final Object listeners = progressiveListeners();
+		if (listeners == null) {
+			return;
+		}
+		
+		final ProgressiveFuture<V> self = (ProgressiveFuture<V>) this;
+		
+		EventExecutor executor = executor();
+		
+		if (executor.inEventLoop()) {
+			if (listeners instanceof GenericProgressiveFutureListener[]) {
+				notifyProgressiveListeners0(
+						self, (GenericProgressiveFutureListener<?>[]) listeners, progress, total);
+			} else {
+				notifyProgressiveListener0(
+						self, (GenericProgressiveFutureListener<ProgressiveFuture<V>>) listeners, progress, total);
+			}
+		} else {
+			if (listeners instanceof GenericProgressiveFutureListener[]) {
+				final GenericProgressiveFutureListener<?>[] array =
+						(GenericProgressiveFutureListener<?>[]) listeners;
+				safeExecute(executor, new Runnable() {
+					@Override
+					public void run() {
+						notifyProgressiveListeners0(self, array, progress, total);
+					}
+				});
+			} else {
+				final GenericProgressiveFutureListener<ProgressiveFuture<V>> l =
+						(GenericProgressiveFutureListener<ProgressiveFuture<V>>) listeners;
+				safeExecute(executor, new Runnable() {
+					@Override
+					public void run() {
+						notifyProgressiveListener0(self, l, progress, total);
+					}
+				});
+			}
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private synchronized Object progressiveListeners() {
+		final GenericFutureListener listener = this.listener;
+		final DefaultFutureListeners listeners = this.listeners;
+		if (listeners == null && listener == null) {
+			return null;
+		}
+		if (listeners != null) {
+			DefaultFutureListeners dfl = listeners;
+			int progressiveSize = dfl.progressiveSize();
+			switch (progressiveSize) {
+			case 0:
+				return null;
+			case 1:
+				for (GenericFutureListener<?> l : dfl.listeners()) {
+					if (l instanceof GenericProgressiveFutureListener) {
+						return l;
+					}
+				}
+				return null;
+			}
+			
+			GenericFutureListener<?>[] array = dfl.listeners();
+			GenericProgressiveFutureListener<?>[] copy = new GenericProgressiveFutureListener[progressiveSize];
+			for (int i = 0, j = 0; j < progressiveSize; i++) {
+				GenericFutureListener<?> l = array[i];
+				if (l instanceof GenericProgressiveFutureListener) {
+					copy[j++] = (GenericProgressiveFutureListener<?>) l; 
+				}
+			}
+			return copy;
+		} else if (listener instanceof GenericProgressiveFutureListener) {
+			return listener;
+		} else {
+			return null;
+		}
+	}
+	
+	private static void notifyProgressiveListeners0(
+			ProgressiveFuture<?> future, GenericProgressiveFutureListener<?>[] listeners, long progress, long total) {
+		for (GenericProgressiveFutureListener<?> l : listeners) {
+			if (l == null) {
+				break;
+			}
+			notifyProgressiveListener0(future, l, progress, total);
+		}
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private static void notifyProgressiveListener0(
+			ProgressiveFuture future, GenericProgressiveFutureListener l, long progress, long total) {
+		try {
+			l.operationProgressed(future, progress, total);
+		} catch (Throwable t) {
+			if (logger.isWarnEnabled()) {
+				logger.warn("An exception was thrown by " + l.getClass().getName() + ".operationProgressed()");
+			}
+		}
+	}
+	
+	private static boolean isCancelled0(Object result) {
+		return result instanceof CauseHolder && ((CauseHolder) result).cause instanceof CancellationException;
+	}
+	
+	private static boolean isDone0(Object result) {
+		return result != null && result != UNCANCELLABLE;
 	}
 	
 	private static final class CauseHolder {
--- src/main/java/netty/common/util/concurrent/FailedFuture.java
+++ src/main/java/netty/common/util/concurrent/FailedFuture.java
@@ -1,5 +1,40 @@
 package netty.common.util.concurrent;
 
-public final class FailedFuture<V> {
+import netty.common.util.internal.ObjectUtil;
+import netty.common.util.internal.PlatformDependent;
 
+public final class FailedFuture<V> extends CompleteFuture<V> {
+	private final Throwable cause;
+	
+	public FailedFuture(EventExecutor executor, Throwable cause) {
+		super(executor);
+		this.cause = ObjectUtil.checkNotNull(cause, "cause");
+	}
+	
+	@Override
+	public Throwable cause() {
+		return cause;
+	}
+	
+	@Override
+	public boolean isSuccess() {
+		return false;
+	}
+	
+	@Override
+	public Future<V> sync() {
+		PlatformDependent.throwException(cause);
+		return this;
+	}
+	
+	@Override
+	public Future<V> syncUninterruptibly() {
+		PlatformDependent.throwException(cause);
+		return this;
+	}
+	
+	@Override
+	public V getNow() {
+		return null;
+	}
 }
